b28cec431114f7ffd5eb80457522b59c136625fe
PixelPunk07 <renzgabuay@gmail.com>
2026-03-01 21:37:00 +0800

    Implement input validation and range limits for grid size
    Adds checks to ensure the grid size is integer
    between 1 and 100. Displays an alert for invalid
    inputs and only calls generatedGrid when
    validation passes.

--- script.js
+++ script.js
@@ -16,4 +16,14 @@ function generateGrid(gridSize) {
 gridBtn.addEventListener("click", () => {
     const askUser = +prompt("Enter grid size");
     if (askUser === null) return;
+
+    const isNotANumber = !Number.isInteger(askUser);
+
+    if ((askUser > 100 || askUser < 1)) {
+        alert("Number greater than 100 or less than 1 is not allowed");
+    } else if (isNotANumber) {
+        alert("Please enter integer only.");
+    } else {
+        generateGrid(askUser);
+    }
 })
