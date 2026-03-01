ffa629efbda1eced6900297be290bfda13585f6a
PixelPunk07 <renzgabuay@gmail.com>
2026-03-01 21:39:09 +0800

    Add the missing percent unit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function generateGrid(gridSize) {
     for (let i = 0; i < gridSize * gridSize; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid");
-        gridItem.style.flex = `1 0 ${boxSize}`;
+        gridItem.style.flex = `1 0 ${boxSize}%`;
 
         container.appendChild(gridItem);
     }
