71716e9d49e4ed6491024caa1ddaa92ac58a8e41
PixelPunk07 <renzgabuay@gmail.com>
2026-03-01 23:48:26 +0800

    Implement a random color generator
    Adds a generateRgbColor function that returns a
    random RGB strings using Math.random(). This will
    be used for the rainbow drawing mode.

--- script.js
+++ script.js
@@ -48,3 +48,11 @@ gridBtn.addEventListener("click", () => {
         generateGrid(askUser);
     }
 })
+
+function generateRgbColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
